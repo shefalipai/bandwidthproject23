301742ff7aabd89d5826d19a84fae50fa9f333b8
shefalipai <spai@bandwidth.com>
2023-05-25 15:36:01 -0400

    notes

--- query.py
+++ query.py
@@ -9,4 +9,6 @@ def get_data():
     df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
     return df.set_index("Region")
 
-
+#create a dashboard that allows other teams to query data with out having to run a sql command. 
+#do it for multiple data sets or make one template where multiple data sets can be replaced and query data from multiple data sets. 
+# make it ui friendly, as people who don't know much about data and queries will be using it. 
